f877846db4a1374f6478dddb6298538ba03f7e2d
Emmanuel3080 <emmanuelabiodun041@gmail.com>
2025-09-09 13:32:22 +0100

    Verify Token

--- Controller/authController.js
+++ Controller/authController.js
@@ -122,4 +122,40 @@ const getSingleUser = async (req, res, next) => {
   }
 };
 
-export { handleSignUP, handleSignIn, handleLogOut, getSingleUser };
+const verifyToken = async (req, res, next) => {
+  let token;
+  try {
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+    if (!token) {
+      return res.status(401).json({
+        Message: "Token Not Provided",
+        Status: "Error",
+      });
+    }
+
+    const { userId } = await jwt.verify(token, process.env.jwtToken);
+
+    const user = await userModel.findById(userId);
+    if (!user) {
+      return res.status(401).json({
+        Message: "User Not Found",
+        Status: "Error",
+      });
+    }
+
+    return res.status(201).json({
+      Message: "Token Is Valid",
+      Status: "Success",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+export { handleSignUP, handleSignIn, handleLogOut, getSingleUser, verifyToken };
--- Router/authRouter.js
+++ Router/authRouter.js
@@ -4,12 +4,14 @@ import {
   handleLogOut,
   handleSignIn,
   handleSignUP,
+  verifyToken,
 } from "../Controller/authController.js";
 const authRouter = express.Router();
 
 authRouter.post("/signup", handleSignUP);
-authRouter.post("/signin", handleSignIn);    
+authRouter.post("/signin", handleSignIn);
 authRouter.post("/logout", handleLogOut);
 authRouter.get("/user/:id", getSingleUser);
+
+authRouter.post("/verify/token", verifyToken);
 export default authRouter;
-  
